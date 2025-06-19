3bed8b25b1922b53a5a7464b4d964691951831a7
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-06-19 18:06:59 +0530

    testing entry().or_insert()

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,13 @@
-use std::collections::btree_map::BTreeMap;
+use std::collections::HashMap;
 
 fn main() {
-    // A client of the bar. They have a blood alcohol level.
-    #[derive(Debug)]
-    struct Person {
-        blood_alcohol: f32,
-    }
+    let mut key_value = HashMap::new();
+    key_value.insert("key1", 10);
+    key_value.insert("key3", 34);
 
-    // All the orders made to the bar, by client ID.
-    let orders = vec![
-        1, 2, 1, 2, 3, 4, 1, 2, 2, 3, 4, 1, 1, 1, 1, 2, 3, 1, 1, 2, 1, 1, 2, 2, 1, 2, 1, 2, 1, 2,
-    ];
+    key_value.entry("key1").or_insert(1);
+    key_value.entry("key2").or_insert(1);
+    key_value.entry("key3").or_insert(1);
 
-    // Our clients.
-    let mut blood_alcohol = BTreeMap::new();
-
-    let mut index = 0;
-    for id in orders {
-        // If this is the first time we've seen this customer, initialize them
-        // with no blood alcohol. Otherwise, just retrieve them.
-        println!("{}: {}", index, id);
-        index += 1;
-        let person = dbg!(
-            blood_alcohol
-                .entry(id)
-                .or_insert(Person { blood_alcohol: 0.0 })
-        );
-
-        // Reduce their blood alcohol level. It takes time to order and drink a beer!
-        person.blood_alcohol *= 0.9;
-
-        // Check if they're sober enough to have another beer.
-        if person.blood_alcohol > 0.3 {
-            // Too drunk... for now.
-            println!("Sorry {id}, I have to cut you off");
-        } else {
-            // Have another!
-            person.blood_alcohol += 0.1;
-        }
-    }
+    println!("{:?}", key_value);
 }
