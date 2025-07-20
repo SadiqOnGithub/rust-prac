5ff0772c8733909fc0c12b8a9c1116a711e944be
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-07-20 15:36:50 +0530

    only use NOT working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use prac_rs::sum;
-// use task::my_task;
-mod task;
+use task::my_task;
+// mod task;
 
 fn main() {
     let result = sum(2, 2);
     println!("The sum is: {}", result);
-    task::my_task();
-    // my_task();
+    // task::my_task();
+    my_task();
 }
