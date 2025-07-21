e0fba6f79b2363c7797514520bfd8c14429b5b04
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-07-21 11:23:55 +0530

    Raw Command Line Arguments (No Libraries)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::env;
+
+fn main() {
+    // This is ALL Rust gives you - a vector of strings
+    let args: Vec<String> = env::args().collect();
+
+    println!("Raw arguments: {:?}", args);
+
+    // If you run: cargo run -- --name Alice --age 25
+    // You get: ["target/debug/myprogram", "--name", "Alice", "--age", "25"]
+
+    // Now YOU have to figure out what each string means!
+}
