47382431322db231253a4671c691d605398c6a8d
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-07-25 14:28:30 +0530

    ittrs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
-    let doubled = (1..=3).map(|x| x * 2); // lazy
-    dbg!(doubled); // won’t show results
+    // let mystery = vec![1, 2, 3].iter().map(|x| x * 2);
+    // let _: () = mystery; // Compiler will tell you the real type!
 
-    // println!("{doubled:q}");
-    println!("Hello, world!");
+    // Can't print iterators directly
+    let iter = vec![1, 2, 3]
+        .into_iter()
+        .map(|x| x * 2)
+        .filter(|x| *x > 3)
+        .skip(1)
+        // .next()
+        // .unwrap()
+        ;
+    println!("{:?}", iter); // ✗ Error!
+
+    // Collect first
+    // let collected: Vec<i32> = iter.collect();
+    // println!("{:?}", collected); // ✓ Works! [2, 4, 6]
 }
