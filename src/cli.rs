41820fbfa49f0f2693c15b8b82920e02b1d12c27
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-07-28 18:01:13 +0530

    same

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,21 @@
 use clap::{Parser, Subcommand};
-use std::fmt::Debug;
+use std::{fmt::Debug, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(name = "git")]
 #[command(about = "A fictional versioning CLI")]
 pub struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
     #[command(subcommand)]
     pub command: Option<Commands>,
 }
@@ -13,7 +24,7 @@ pub struct Cli {
 pub enum Commands {
     /// Adds files to the repository
     Add {
-        #[clap(short, long, default_value_t = true)]
+        #[arg(short, long, default_value_t = true)]
         recursive: bool,
         /// Files to add
         #[arg(required = true)]
