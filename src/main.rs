025790ca378ded76782589def0f4b1cf33678181
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2025-11-23 11:18:30 +0530

    dereferenceing with mut variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // immutable reference
+    let x = 5;
+    let r = &x;
+    assert_eq!(5, *r);
+
+    // mutable reference
+    let mut y = 10;
+    // {
+    let mut_ref = &mut y;
+    *mut_ref += 5;
+    assert_eq!(&mut 15, mut_ref);
+    println!("mut_ref: {}", mut_ref);
+    println!("y: {}", y); // Now we can access y
+    // }
+    assert_eq!(15, y);
 }
